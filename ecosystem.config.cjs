a4e7fba45c256acca1cef0f8cd4826ae005c6ce5
AzmiHwamed <70154469+AzmiHwamed@users.noreply.github.com>
2026-08-08 13:32:43 +0100

    changes

---
+++ ecosystem.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "translate-app-admin",
+      script: "serve",
+      env: {
+        PM2_SERVE_PATH: "dist",
+        PM2_SERVE_PORT: 5024,
+        PM2_SERVE_SPA: "true",
+        PM2_SERVE_HOMEPAGE: "/index.html",
+      },
+    },
+  ],
+};
